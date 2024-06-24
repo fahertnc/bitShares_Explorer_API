891ea8847ac7e678a9198c3264069aeb3a418b7d
faherTNC <147723828+faherTNC@users.noreply.github.com>
2024-06-24 12:25:24 +0400

    Update limiter.py

--- services/limiter.py
+++ services/limiter.py
@@ -4,7 +4,7 @@ from redis import Redis
 
 limiter = Limiter(
     key_func=get_remote_address,
-    storage_uri="redis:localhost:6379",
+    storage_uri="redis://localhost:6379",
     default_limits=[]
 )
 def init_app(app):
